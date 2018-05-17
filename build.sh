5cb524725748b7ea45b7bc1f2712076ee7994954
Ryan Anthony <ryandanthony@hotmail.com>
2018-05-17 16:14:05 -0600

    updates to try to push automatically do docker hub

--- build.sh
+++ build.sh
@@ -4,6 +4,17 @@
 set -e
 cd ./src/Projection.Utils
 dotnet publish -o ./pub
-source="$pwd/pub"
-docker build --build-arg source=$source . 
+source="${pwd}/pub"
+image="eventstorecontrib/eventstore-projection-utils"
+versiontag="${image}:${APPVEYOR_BUILD_VERSION}"
+latesttag="${image}:latest"
 
+echo $source
+echo $latesttag
+echo $versiontag
+
+docker build --build-arg source=$source -t $latesttag -t $versiontag . 
+
+docker login -u=$DOCKER_USER -p=$DOCKER_PASS
+docker push $versiontag
+docker push $latesttag
