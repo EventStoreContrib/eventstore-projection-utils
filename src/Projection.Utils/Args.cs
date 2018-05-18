3bc77b7b1c133eccb678389174d707eb0e93a004
Ryan Anthony <ryandanthony@hotmail.com>
2018-05-18 08:53:05 -0600

    don't make args required when they have default values

--- src/Projection.Utils/Args.cs
+++ src/Projection.Utils/Args.cs
@@ -16,19 +16,19 @@ namespace EventStoreContrib.Utils
 
     public class BaseArgs
     {
-        [ArgRequired, ArgDescription("The ip address of the Event Store instance"), ArgShortcut("ip")]
+        [ArgDescription("The ip address of the Event Store instance"), ArgShortcut("ip")]
         public string IpAddress { get; set; }
 
-        [ArgRequired, ArgDescription("The port number (web) of the Event Store instance"), ArgShortcut("po"), ArgDefaultValue("2113")]
+        [ArgDescription("The port number (web) of the Event Store instance"), ArgShortcut("po"), ArgDefaultValue("2113")]
         public string Port { get; set; }
 
-        [ArgRequired, ArgDescription("The username to connect to the Event Store instance with"), ArgShortcut("u"), ArgDefaultValue("admin")]
+        [ArgDescription("The username to connect to the Event Store instance with"), ArgShortcut("u"), ArgDefaultValue("admin")]
         public string Username { get; set; }
 
-        [ArgRequired, ArgDescription("The password to connect to the Event Store instance with"), ArgShortcut("p"), ArgDefaultValue("changeit")]
+        [ArgDescription("The password to connect to the Event Store instance with"), ArgShortcut("p"), ArgDefaultValue("changeit")]
         public string Password { get; set; }
 
-        [ArgRequired, ArgDescription("The timeout to use for operations"), ArgShortcut("to"), ArgDefaultValue(3000)]
+        [ArgDescription("The timeout to use for operations"), ArgShortcut("to"), ArgDefaultValue(3000)]
         public int OperationTimeout { get; set; }
     }
 }
